e6b8251c95e42103bbc4347a97ba87580cd0a84a
lunavath-sandhya <sandhyalunavath2005@gmail.com>
2025-08-27 13:02:27 +0530

    Add files via upload

---
+++ s8.py
@@ -0,0 +1,12 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:/Users/Hp/AppData/Local/Programs/Python/Python312/s8.py ======
+enter num115
+enter num220
+a&b: 4
+a|b: 31
+a^b: 27
+~a: -16
+a<<1: 30
+b>>1: 10
